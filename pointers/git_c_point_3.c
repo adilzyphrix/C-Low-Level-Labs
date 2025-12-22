9242bd484e9ac2b9a9c4d998e229af758c4a4c58
Adil Ahamed <ahamedadil480@gmail.com>
2025-12-22 17:01:46 +0530

    Implementing jagged integer arrays using array of pointers

--- pointers/git_c_point_3.c
+++ pointers/git_c_point_3.c
@@ -1,26 +1,39 @@
+/*
+    Implementing dynamic memory allocation
+    to allocate space for 3 jagged arrays(of different sizes).
+    Taking input from the user and then printing the array elements.
+*/
 #include <stdio.h>
-#include <string.h>
-#define MAX_LEN 20
-#define NUM_STRINGS 3
-void makeStringsUppercase(char (*array)[MAX_LEN],int count){
-    for(int i=0;i<count;i++){
-        for(int j=0;array[i][j]!='\0';j++){
-            if(array[i][j] >= 'a' && array[i][j] <= 'z'){
-                array[i][j]-=32;
-            }
-        }
-    }
-    printf("\n");
-}
+#include <stdlib.h>
 int main()
 {
-    char arr[NUM_STRINGS][MAX_LEN] = {
-        "Systems programming",
-        "linux internals",
-        "c language"
-    };
-    for(int i=0;i<NUM_STRINGS;i++) printf("%s\n",arr[i]);
-    makeStringsUppercase(arr,NUM_STRINGS);
-    for(int i=0;i<NUM_STRINGS;i++) printf("%s\n",arr[i]);
+    int *arr[3] = {NULL}; //initializing to NULL
+    int sizes[] = {3,4,5}; //sizes of each jagged arrays
+
+    for(int i=0;i<3;i++){
+        arr[i] = (int *)malloc(sizes[i] * sizeof(int)); //dynamic memory allocation
+        if(arr[i]==NULL) return 1; //in case of unsuccessful allocation
+    }
+
+    for(int i=0;i<3;i++){
+        printf("Row %d elements(maximum %d integers allowed): ",i,sizes[i]);
+        for(int j=0;j<sizes[i];j++){
+            scanf(" %d",&arr[i][j]); //taking integers as inputs for each jagged array
+        }
+    }
+
+    printf("The jagged arrays are as follows\n");
+    for(int i=0;i<3;i++){
+        for(int j=0;j<sizes[i];j++)
+            printf("%d ",arr[i][j]);
+        printf("\n");
+    }
+
+    for(int i=3-1;i>=0;i--){
+        free(arr[i]); //freeing memory in reverse order
+        arr[i] = NULL; //preventing dangling pointers
+    }
+
+>>>>>>> 21ca3bd (Implementing jagged integer arrays using array of pointers)
     return 0;
 }
